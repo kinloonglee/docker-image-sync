03643ae0df547dc23dd08bf373a9337e8b550ee4
303882175@qq.com <303882175@qq.com>
2025-11-30 12:03:04 +0800

    1

--- sync.sh
+++ sync.sh
@@ -56,8 +56,8 @@ while IFS= read -r image; do
  
     # 拉取原始镜像
     echo "Pulling original image: ${image}..."
-    # docker pull --platform arm64 "${image}"
-    docker pull  "${image}"
+    docker pull --platform arm64 "${image}"
+    # docker pull  "${image}"
  
     # 打上阿里云 ACR 的标签
     echo "Tagging image ${image} to ${target_full_image_path}..."
